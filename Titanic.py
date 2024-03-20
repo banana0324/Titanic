4e3a2c2c381ff3028e4be29b716e761cac5759c2
HAO <david0324t@gmail.com>
2024-03-21 03:15:08 +0800

    modified:   Titanic.py 	modified:   result.csv

--- Titanic.py
+++ Titanic.py
@@ -17,7 +17,7 @@ def EDA():
     print(titanic.head())
     print(titanic.shape)
     print(titanic.info())
-    print(titanic.describe)
+    print(titanic.describe())
 # EDA()
 
 def dataprocessing():
@@ -31,10 +31,11 @@ def pieplot():
 
     plt.pie(gender['PassengerId'], labels=['female','male'], autopct='%1.1f%%', startangle=100, colors=['pink','skyblue'])
     plt.show()
+# pieplot()
 # ------------------------2--------------------------------------------------------------
 # 看Age與Sex的關係
 # sb.catplot(x = "Sex",y = "Age",data=titanic,kind = 'box')
-
+# plt.show()
 # 觀察缺失資料在船艙等級(Pclass)的分布
 # sb.countplot(x = titanic["Pclass"], hue = titanic['Age'].isnull())
 # plt.show()
@@ -42,7 +43,6 @@ def pieplot():
 # 觀察Age和Survived的相關性
 # index_survived = (titanic["Age"].isnull()==False)&(titanic["Survived"]==1)
 # index_died = (titanic["Age"].isnull()==False)&(titanic["Survived"]==0)
-
 # sb.distplot( titanic.loc[index_survived ,'Age'], bins=20, color='blue', label='Survived' )
 # sb.distplot( titanic.loc[index_died ,'Age'], bins=20, color='red', label='Survived' )
 # plt.show()
@@ -63,75 +63,65 @@ def select_name(s):
 name = titanic['Title'].apply(select_name)
 dummy = pd.get_dummies(name)
 traindf = pd.concat([titanic, dummy], axis=1)
+print(traindf)
 
-# # 計算每個 Title 的年齡平均值
+# 計算每個 Title 的年齡平均值
 Age_Mean = titanic[['Title','Age']].groupby( by=['Title'] ).mean()
-
 Age_Mean.columns = ['Age_Mean']
 Age_Mean.reset_index( inplace=True )
+# print(Age_Mean)
 
-titanic=titanic.reset_index() #重整index
+# Title 年齡補值
+titanic=titanic.reset_index()
 for i in range(len(titanic["Age"].isnull())):
     if titanic["Age"].isnull()[i]==True:
         for j in range(len(Age_Mean.Title)):
             if titanic["Title"][i]==Age_Mean.Title[j]:
                 titanic["Age"][i]=Age_Mean.Age_Mean[j]
 
-# # dataprocessing()
+# dataprocessing()
 
-# def histogramplot():
-#     sb.histplot(x='Age',data=titanic)
-#     plt.show()
-# # histogramplot()
+def survivePlot():
+    cols = ['Sex','Pclass','SibSp','Parch','Embarked']
 
-# def survivePlot():
-#     cols = ['Sex','Pclass','SibSp','Parch','Embarked']
+    n_rows = 2
+    n_cols = 3
 
-#     n_rows = 2
-#     n_cols = 3
+    # The subplot grid and figure size of each graph
+    fig, axs = plt.subplots(n_rows,n_cols,figsize = (n_cols * 3.2, n_rows * 3.2))
 
-#     # The subplot grid and figure size of each graph
-#     fig, axs = plt.subplots(n_rows,n_cols,figsize = (n_cols * 3.2, n_rows * 3.2))
-
-#     for r in range(0, n_rows):
-#         for c in range(0, n_cols):
+    for r in range(0, n_rows):
+        for c in range(0, n_cols):
     
-#             i = r * n_cols + c  # index to go through the number of columns
-#             if i < 5:
-#                 ax = axs[r][c] #show where to position each sub plots
-#                 sb.countplot(x = titanic[cols[i]], hue = titanic['Survived'], ax= ax)
-#                 ax.set_title(cols[i])
-#                 ax.legend(title = 'survived', loc = 'upper right')
+            i = r * n_cols + c  # index to go through the number of columns
+            if i < 5:
+                ax = axs[r][c] #show where to position each sub plots
+                sb.countplot(x = titanic[cols[i]], hue = titanic['Survived'], ax= ax)
+                ax.set_title(cols[i])
+                ax.legend(title = 'survived', loc = 'upper right')
 
-#     plt.tight_layout()
-#     plt.show()
+    plt.tight_layout()
+    plt.show()
 
-# # 描述性統計
-# # survivePlot()
-# # 大致可以觀察出
+# survivePlot()
 
-# # 女性存活的機會比男性來得高
-# # 頭等艙存活機會較高
-# # 可能有帶兄弟姊妹、老婆丈夫的乘客存活機會較高
-# # 有帶小孩父母親的存活機會較高
-# # 從S碼頭出發有可能艙位比較低，存活機會較低
 
-# def genderSurvive():
-#     titanic.groupby('Sex')[['Survived']].mean()
-#     titanic.pivot_table('Survived',index = 'Sex', columns = 'Pclass')
+def genderSurvive():
+    titanic.groupby('Sex')[['Survived']].mean()
+    titanic.pivot_table('Survived',index = 'Sex', columns = 'Pclass')
 
-#     age = pd.cut(titanic['Age'],[0, 18, 80])
-#     titanic.pivot_table('Survived',['Sex',age], 'Pclass')
-# # genderSurvive()
+    age = pd.cut(titanic['Age'],[0, 18, 40, 80])
+    print(titanic.pivot_table('Survived',['Sex',age], 'Pclass'))
+# genderSurvive()
 
-# def pClassSurvive():
-#     plt.scatter(titanic['Fare'],titanic['Pclass'], color = 'purple', label = 'Passenger Paid')
-#     plt.ylabel('Class')
-#     plt.xlabel('Price / Fare')
-#     plt.title('Price of Each Class')
-#     plt.legend()
-#     plt.show()
-# # pClassSurvive(
+def pClassSurvive():
+    plt.scatter(titanic['Fare'],titanic['Pclass'], color = 'blue', label = 'Passenger Paid')
+    plt.ylabel('Class')
+    plt.xlabel('Price / Fare')
+    plt.title('Price of Each Class')
+    plt.legend()
+    plt.show()
+# pClassSurvive()
 
 # # --------------------------資料分割------------------------------------
 features = ['PassengerId','Survived','Pclass','Sex','Age','SibSp','Parch','Fare','Embarked','Title']
@@ -142,8 +132,10 @@ y = titanic.Survived
 features_dummy = titanic[['Embarked','Sex','Title']]
 features_dummy = pd.get_dummies(features_dummy,columns=['Embarked','Sex','Title'])
 X = pd.concat([X ,features_dummy],axis=1)
+# print(X)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2)
+
 
 traindf = X_train.drop(['PassengerId','Title','Title_Capt', 'Title_Col', 'Title_Don', 'Title_Dr', 'Title_Jonkheer',
        'Title_Lady', 'Title_Major','Title_Mlle',
@@ -157,9 +149,9 @@ testdf = X_test.drop(['Title','Survived','Title_Capt', 'Title_Col', 'Title_Don',
 
 # 相關係數
 # print(traindf.corr())
-# plt.figure(figsize=(14, 14))
+plt.figure(figsize=(14, 14))
 
-# sb.heatmap(traindf.corr(), annot=True, cmap='RdBu')
+sb.heatmap(traindf.corr(), annot=True, cmap='RdBu')
 # plt.show()
 
 # --------------------------建模--------------------------------
